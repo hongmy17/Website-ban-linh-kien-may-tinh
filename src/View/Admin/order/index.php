8ec6c5a320a9b7e50d769210cd94fd174e79e94d
hongmy17 <hongmy172a@gmail.com>
2025-11-27 16:20:31 +0700

    [Admin][HTML] - Order/Index: Xóa các ô checkbox trang giao diện danh sách đơn hàng

--- src/View/Admin/order/index.php
+++ src/View/Admin/order/index.php
@@ -5,11 +5,6 @@
     <thead>
       <tr class="border-bottom">
         <th>
-          <span class="ml-1">
-            <input class="form-check-input" type="checkbox" id="checkbox-all" />
-          </span>
-        </th>
-        <th>
           <span class="ml-1">STT</span>
         </th>
         <th>
@@ -26,11 +21,6 @@
     <tbody>
       <tr class="border-bottom">
         <td>
-          <div class="p-2 ps-0">
-            <input class="form-check-input" type="checkbox" name="id[]" value="" />
-          </div>
-        </td>
-        <td>
           <div class="p-2">1</div>
         </td>
         <td>
@@ -57,73 +47,6 @@
           </div>
         </td>
       </tr>
-      <tr class="border-bottom">
-        <td>
-          <div class="p-2 ps-0">
-            <input class="form-check-input" type="checkbox" name="id[]" value="" />
-          </div>
-        </td>
-        <td>
-          <div class="p-2">1</div>
-        </td>
-        <td>
-          <a href="#"
-            class="p-2 d-flex flex-row align-items-center mb-2 text-decoration-none text-reset">
-            <img src="/public/assets/images/admin/default-user-image.webp" width="40"
-              class="me-3 rounded-circle" />
-            <div class="d-flex flex-column ml-2">
-              <span class="d-block font-weight-bold">Nguyễn Đặng Hồng Mỹ</span>
-              <small class="text-muted">example@gmail.com</small>
-            </div>
-          </a>
-        </td>
-        <td>
-          <div class="p-2">
-            <span class="status text-success">&bull;</span> Đã thanh toán
-          </div>
-        </td>
-        <td>
-          <div class="p-2 icons">
-            <a href="#" class="edit text-decoration-none mx-3">
-              <i class="fas fa-info"></i>
-            </a>
-          </div>
-        </td>
-      </tr>
-      <tr class="border-bottom">
-        <td>
-          <div class="p-2 ps-0">
-            <input class="form-check-input" type="checkbox" name="id[]" value="" />
-          </div>
-        </td>
-        <td>
-          <div class="p-2">1</div>
-        </td>
-        <td>
-          <a href="#"
-            class="p-2 d-flex flex-row align-items-center mb-2 text-decoration-none text-reset">
-            <img src="/public/assets/images/admin/default-user-image.webp" width="40"
-              class="me-3 rounded-circle" />
-            <div class="d-flex flex-column ml-2">
-              <span class="d-block font-weight-bold">Nguyễn Đặng Hồng Mỹ</span>
-              <small class="text-muted">example@gmail.com</small>
-            </div>
-          </a>
-        </td>
-        <td>
-          <div class="p-2">
-            <span class="status text-success">&bull;</span> Đã thanh toán
-          </div>
-        </td>
-        <td>
-          <div class="p-2 icons">
-            <a href="#" class="edit text-decoration-none mx-3">
-              <i class="fas fa-info"></i>
-            </a>
-          </div>
-        </td>
-      </tr>
-
     </tbody>
   </table>
 
