4953eb03de828b01e1659e76748ea7383dd8af2e
hongmy17 <hongmy172a@gmail.com>
2025-11-27 15:46:33 +0700

    [Client][HTML] - Account/Login: Gộp giao diện trang login từ nhánh CongXum

---
+++ src/View/Client/account/login.php
@@ -0,0 +1,24 @@
+<div class="login-box">
+    <h2>Đăng Nhập</h2>
+
+
+    <div class="input-group">
+        <label>Email</label>
+        <input type="email" placeholder="Nhập email..." />
+    </div>
+
+
+    <div class="input-group">
+        <label>Mật khẩu</label>
+        <input type="password" placeholder="Nhập mật khẩu..." />
+    </div>
+
+
+    <button class="btn-login">Đăng nhập</button>
+
+
+    <div class="extra-link">
+        <p><a href="#">Quên mật khẩu?</a></p>
+        <p>Chưa có tài khoản? <a href="#">Đăng ký</a></p>
+    </div>
+</div>
