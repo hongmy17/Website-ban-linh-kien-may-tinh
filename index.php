325389ffb501ce573a725a98e8e4c48f379ba0e4
hongmy17 <hongmy172a@gmail.com>
2025-12-03 12:43:19 +0700

    [DATABASE]: Chuyển sang dùng database trên host, thêm bảo mật thông tin database.

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once "vendor/autoload.php";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 use App\Router;
 
--- src/Model/Model.php
+++ src/Model/Model.php
@@ -1,26 +1,31 @@
 <?php
 
 namespace App\Model;
+
 use App\Interface\Validate;
 use PDO;
 
-abstract class Model implements Validate {
+abstract class Model implements Validate
+{
   protected $table;
   protected $connection;
   private Database $database;
 
-  public function __construct() {
-    $this->database = new Database("localhost", "xdtw2", "root", "mysql");
+  public function __construct()
+  {
+    $this->database = new Database($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"]);
     $this->connection = $this->database->connect();
   }
 
-  public function findAll() {
+  public function findAll()
+  {
     $sql = "SELECT * FROM {$this->table}";
     $rows = $this->connection->query($sql);
     return $rows->fetchAll(PDO::FETCH_ASSOC);
   }
 
-  public function find($id) {
+  public function find($id)
+  {
     $sql = "SELECT * FROM {$this->table} WHERE id = ?";
     $stmt = $this->connection->prepare($sql);
     $stmt->execute([$id]);
